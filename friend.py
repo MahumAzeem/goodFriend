f12c01da8910a354123e7429da03a1119ca58ab4
Kathryn Rieb <kanrieb@gmail.com>
2021-01-09 10:26:42 -0800

    Finalized db.py

--- friend.py
+++ friend.py
@@ -9,6 +9,7 @@ class Friend(BaseModel):
     birthdate: datetime.date
     allergies: Optional[str]
     pronouns: Optional[str]
+    phone: Optional[str]
     avatar: Optional[str]
     optional1: Optional[Dict[str,str]]
     optional2: Optional[Dict[str,str]]
